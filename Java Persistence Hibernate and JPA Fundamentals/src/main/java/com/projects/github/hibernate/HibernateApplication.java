dc38a4845cc2295862350987786d25199079a682
github9582816659 <github9582816659@gmail.com>
2021-03-15 14:51:38 +0530

    update

--- Java Persistence Hibernate and JPA Fundamentals/src/main/java/com/projects/github/hibernate/HibernateApplication.java
+++ Java Persistence Hibernate and JPA Fundamentals/src/main/java/com/projects/github/hibernate/HibernateApplication.java
@@ -1,13 +1,46 @@
 package com.projects.github.hibernate;
 
+import com.projects.github.hibernate.model.Book;
+import com.projects.github.hibernate.model.Chapter;
+import com.projects.github.hibernate.model.Publisher;
+import com.projects.github.hibernate.service.BookStoreService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class HibernateApplication {
+public class HibernateApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		BookStoreService bookStoreService = new BookStoreService();
+
+		//persisting object graph
+		/*Publisher publisher = new Publisher("MANN", "Manning Publications Co.");
+
+		Book book = new Book("9781617290459", "Java Persistence with Hibernate, Second Edition", publisher);
+
+		List<Chapter> chapters = new ArrayList<Chapter>();
+		Chapter chapter1 = new Chapter("Introducing JPA and Hibernate", 1);
+		chapters.add(chapter1);
+		Chapter chapter2 = new Chapter("Domain Models and Metadata", 2);
+		chapters.add(chapter2);
+
+		book.setChapters(chapters);
+
+		bookStoreService.persistObjectGraph(book);*/
+
+
+		//retrieving object graph
+		Book book = bookStoreService.retrieveObjectGraph("9781617290459");
+		System.out.println(book);
+
+	}
 }
